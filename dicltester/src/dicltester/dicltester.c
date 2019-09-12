e5f279f44df7070ed13d668b1c3ded715993d396
Daniel Hams <daniel.hams@gmail.com>
2019-09-12 02:06:52 +0100

    Test program

---
+++ dicltester/src/dicltester/dicltester.c
@@ -0,0 +1,15 @@
+#include <dicl/dicl.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char**argv)
+{
+    printf("Dicltester called.\n");
+
+    printf("libdicl version is ->%s\n",
+           libdicl_getversion());
+
+    return 0;
+}
