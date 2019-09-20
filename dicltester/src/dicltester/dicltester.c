efaa6aa33b49c9a9c532095a80e4be051ca9419a
Daniel Hams <daniel.hams@gmail.com>
2019-09-20 08:56:54 +0100

    Updated with ptrdiff_t test too

--- dicltester/src/dicltester/dicltester.c
+++ dicltester/src/dicltester/dicltester.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 int main(int argc, char**argv)
 {
@@ -15,8 +16,13 @@ int main(int argc, char**argv)
 
     printf("printf is %s\n", "printf");
 
-    printf("The size with %%ld is %ld\n", somesize);
-    printf("The size with %%zd is %zd\n", somesize);
+    printf("The size_t with %%ld is %ld\n", somesize);
+    printf("The size_t with %%zd is %zd\n", somesize);
+
+    ptrdiff_t ptrd = 0;
+
+    printf("The ptrdiff_t with %%ld is %ld\n", ptrd);
+    printf("The ptrdiff_t with %%td is %td\n", ptrd);
 
     return 0;
 }
