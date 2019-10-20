11f7a6c573b9250623f318b24e2146829af50711
Daniel Hams <daniel.hams@gmail.com>
2019-10-20 08:04:45 +0100

    Update to 0.1.2 and support make dist properly now.

--- libdicl/cleanupacfiles.sh
+++ libdicl/cleanupacfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf COPYING INSTALL aclocal.m4 autom4te.cache config.guess config.h.in config.sub configure install-sh ltmain.sh missing config.h.in~ Makefile Makefile.in config.h config.log config.status lib*.pc depcomp compile libtool src/.deps src/Makefile src/Makefile.in stamp-h1 macros/libtool.m4 macros/lt*.m4 config.rpath test-driver
+rm -rf COPYING INSTALL aclocal.m4 autom4te.cache config.guess config.h.in config.sub configure install-sh ltmain.sh missing config.h.in~ Makefile Makefile.in config.h config.log config.status lib*.pc depcomp compile libtool src/.deps src/Makefile src/Makefile.in stamp-h1 macros/libtool.m4 macros/lt*.m4
 # These get left when extra sources directories get added under src
 find . -name "\.deps" -exec rm -rf {} \;
 find . -name "*~" -exec rm -f {} \;
