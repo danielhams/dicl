a5dbfc4c2a52d8fec1e7f0e14fd50eecf8a52f5f
Daniel Hams <daniel.hams@gmail.com>
2020-01-01 11:20:05 +0000

    pselect: add prototype to repl_select.h

--- libdicl/src/repl_headers/repl_select.h
+++ libdicl/src/repl_headers/repl_select.h
@@ -1,6 +1,20 @@
 #ifndef LIBDICL_REPL_SELECT_H
 #define LIBDICL_REPL_SELECT_H
 
+#if defined(__GNUC__)
+#include_next <unistd.h>
+#include_next <sys/types.h>
+#include_next <bstring.h>
+#include_next <sys/time.h>
+#include_next <sys/signal.h>
+#else
+#include "/usr/include/unistd.h"
+#include "/usr/include/sys/types.h"
+#include "/usr/include/bstring.h"
+#include "/usr/include/sys/time.h"
+#include "/usr/include/sys/signal.h"
+#endif
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
@@ -8,6 +22,11 @@ extern "C" {
 #undef select
 #define select rpl_select
 extern int rpl_select(int, fd_set *, fd_set *, fd_set *, struct timeval *);
+
+/* New - pselect for irix */
+extern int pselect(int, fd_set *, fd_set *, fd_set *,
+		   const struct timespec *restrict,
+		   const sigset_t *restrict);
 
 #if defined(__cplusplus)
 }
