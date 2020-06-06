35e528af067adabee989bab4c8b19d06b4a9f3d2
Daniel Hams <daniel.hams@gmail.com>
2020-06-06 16:46:13 +0100

    libdicl: fix up printf replacements to hint they are formatted like printf - so gcc can validate the arguments

--- libdicl/src/repl_headers/stdio.h
+++ libdicl/src/repl_headers/stdio.h
@@ -11,33 +11,65 @@
 extern "C" {
 #endif
 
-extern int asprintf(char **,const char *, ...);
+extern int asprintf(char **,const char *, ...)
+#if defined(__GNUC__)
+  __attribute__ ((format (__printf__, 2, 3)))
+#endif
+;
 
 #undef sprintf
 #define sprintf rpl_sprintf
-extern int rpl_sprintf(char *,const char *, ...);
+extern int rpl_sprintf(char *,const char *, ...)
+#if defined(__GNUC__)
+  __attribute__ ((format (__printf__, 2, 3)))
+#endif
+;
 
 #undef printf
 #define printf rpl_printf
-extern int rpl_printf(const char *, ...);
+extern int rpl_printf(const char *, ...)
+#if defined(__GNUC__)
+  __attribute__ ((format (__printf__, 1, 2)))
+#endif
+;
 
 #undef fprintf
 #define fprintf rpl_fprintf
-extern int rpl_fprintf(FILE *, const char *, ...);
+extern int rpl_fprintf(FILE *, const char *, ...)
+#if defined(__GNUC__)
+  __attribute__ ((format (__printf__, 2, 3)))
+#endif
+;
 
 #undef vfprintf
 #define vfprintf rpl_vfprintf
-extern int rpl_vfprintf(FILE *, const char *, va_list);
+extern int rpl_vfprintf(FILE *, const char *, va_list)
+#if defined(__GNUC__)
+  __attribute__ ((format (__printf__, 2, 0)))
+#endif
+;
 
 #undef vsprintf
 #define vsprintf rpl_vsprintf
-extern int rpl_vsprintf(char *,const char *, va_list);
+extern int rpl_vsprintf(char *,const char *, va_list)
+#if defined(__GNUC__)
+  __attribute__ ((format (__printf__, 2, 0)))
+#endif
+;
 
 #undef vsnprintf
 #define vsnprintf rpl_vsnprintf
-extern int rpl_vsnprintf(char *,size_t,const char *, va_list);
+extern int rpl_vsnprintf(char *,size_t,const char *, va_list)
+#if defined(__GNUC__)
+  __attribute__ ((format (__printf__, 3, 0)))
+#endif
+;
 
-extern int vasprintf(char**, const char*, va_list);
+extern int vasprintf(char**, const char*, va_list)
+#if defined(__GNUC__)
+  __attribute__ ((format (__printf__, 2, 0)))
+#endif
+;
 
 /* Missing pieces */
 
